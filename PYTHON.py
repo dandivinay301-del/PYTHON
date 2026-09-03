04d5cb56c9b5ffbc1ae3723a7729b9fddbe5972b
dandivinay301-del <dandivinay301@gmail.com>
2026-09-03 12:47:31 +0530

    Add files via upload

---
+++ PYTHON.py
@@ -0,0 +1,8 @@
+salary = float(input("Enter basic salary: "))
+Ha = salary * 20 / 100
+da = salary * 10 / 100
+total_salary = salary + Ha + da
+print("Basic Salary =", salary)
+print("House Allowance (HA) =", Ha)
+print("Dearness Allowance (DA) =", da)
+print("Total Salary =", total_salary)
